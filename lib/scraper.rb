c079e87c9d81d6b1eb82499df282c0ca30bad4a8
Azraf Chowdhury <achowdhur@ltu.edu>
2019-01-02 06:51:39 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,6 +6,9 @@ class Scraper
   def self.scrape_index_page(index_url)
     html = File.read('./fixtures/student-site/index.html')
     Scraper = Nokogiri::HTML(html)
+
+    Scraper.css("student-card").each do |student|
+      
   end
 
   def self.scrape_profile_page(profile_url)

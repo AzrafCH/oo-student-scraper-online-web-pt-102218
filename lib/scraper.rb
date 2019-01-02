6d44fcd2588c458788d1656eb6fb4ac6151afc67
Azraf Chowdhury <achowdhur@ltu.edu>
2019-01-02 06:59:54 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,6 +12,10 @@ class Scraper
       name = student.css(".student-name").text
       location = student.css(".student-location").text
       profile_url = student.css("a").attribute("href").value
+      student_info = {:name => name, :location => location, :profile_url => profile_url}
+      students << student_info
+    end 
+    
 
 
   end
